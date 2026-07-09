4c6cecc2ee43f5f64c6a342526a7ea64f293bb43
Anoopkumar001 <anoopkumar799241@gmail.com>
2026-07-09 11:45:08 +0530

    Update backend API URL

--- clients/src/utils/axios.js
+++ clients/src/utils/axios.js
@@ -1,20 +1,18 @@
-
 import axios from "axios";
 
-const api=axios.create({
-    baseURL:'http://localhost:5000/api',
-    headers:{
-        'Content-Type':'application/json',
+const api = axios.create({
+    baseURL: "https://bookingevent-backend.onrender.com/api",
+    headers: {
+        "Content-Type": "application/json",
     },
 });
 
-api.interceptors.request.use((config)=>{
-    const token = localStorage.getItem('token');
-    if(token){
-        config.headers.Authorization=`Bearer ${token}`;
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
 });
-
 
 export default api;
